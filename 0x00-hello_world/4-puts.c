a028b5d46674793e5c5da47c2e4052929d879ebf
babaoni147 <obahedwin@gmail.com>
2023-08-14 10:17:14 -0700

    first C-programm

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - Entery point
- * Returm (0)
+ * main - Entry point
+ * Return: 0 (success)
  */
 int main(void)
 {
-	puts("\"Programming is like builging a multilingual puzzle");
-	return (0);
-}	
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
