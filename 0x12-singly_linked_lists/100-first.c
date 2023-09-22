6b0688dacf1fca78c7a2fa698ccaa2492e278277
Obah_Edwin <obahedwin@ymail.com>
2023-09-22 16:33:10 -0700

    The Hare and the Tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void tortoise(void) __attribute__((constructor));
+
+/**
+ * tortoise - function that apply constructor attribute
+ * and its implementation.
+ */
+void tortoise(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!");
+}
